3610b8caf2e25000c0e2bccec1710b1762ba89fa
Igor <korn.shon@gmail.com>
2025-12-29 01:05:15 +0500

    update

--- Source/FirstProject/FirstProject.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstProject, "FirstProject" );
-
-DEFINE_LOG_CATEGORY(LogFirstProject)
--- Source/FirstProject/FirstProjectGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FirstProjectGameMode.h"
-
-AFirstProjectGameMode::AFirstProjectGameMode()
-{
-	// stub
-}
